3751020792fc9638d10c43fb90d16fae0aeaca57
kapoorvedant18 <64012932+kapoorvedant18@users.noreply.github.com>
2020-11-08 20:08:28 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,11 @@
+class Dustbin {
+    constructor(x,y,width,height)
+    options = {
+        isStatic:true,
+    }
+    this.body=Bodies.rectangle(x,y,width,height,options); 
+    this.width=width;
+    this.height=height;
+}   
+
+    
